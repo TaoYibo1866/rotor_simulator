b48e479c5b5e9331bb0a0984c99bf40524e53f01
TaoYibo1866 <821869740@qq.com>
2020-12-30 18:27:18 +0800

    first commit

---
+++ rotor_simulator/src/rotor2_joy_node.cpp
@@ -0,0 +1,24 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>
+
+using sensor_msgs::Joy;
+
+ros::Publisher cmd_pub;
+
+void joyCb(Joy joy)
+{
+    Joy cmd;
+    cmd.axes.push_back(joy.axes[3] * M_PI / 3);
+    cmd.axes.push_back(-joy.axes[4] * M_PI / 4);
+    cmd_pub.publish(cmd);
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "rotor2_joy");
+    ros::NodeHandle nh;
+    ros::Subscriber joy_sub = nh.subscribe<Joy>("/joy", 1, joyCb);
+    cmd_pub = nh.advertise<Joy>("rotor2_angle_cmd", 1);
+    ros::spin();
+    return 0;
+}
